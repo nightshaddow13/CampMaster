06f1876da43da252b508e1d77a15bd0a876744b5
Xavier Goss <41086688+nightshaddow13@users.noreply.github.com>
2020-10-30 20:21:26 -0400

    Added Mobile UI
    There are sub UIs for Android and IOS.  Android will be the first OS supported on launch.

---
+++ Repos/com.CampMaster.Mobile.UI/com.CampMaster.Mobile.UI.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace com.CampMaster.Mobile.UI.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
